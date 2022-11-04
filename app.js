031184aaeec4ae700dbe2d0e1524d296de35bf3e
Chloe905 <cchloehsieh@gmail.com>
2022-11-04 11:15:34 +0800

    initilize project

---
+++ app.js
@@ -0,0 +1,48 @@
+// 1. Insert Express by require
+const express = require('express')
+const app = express()
+// 2. Define server related variable
+const port = 3000
+// 5. Insert express-handlebars by require
+const exphbs = require('express-handlebars')
+// 8. Require package used in the project
+const restaurantList = require('./restaurant.json').results
+
+// 6. Set template Engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// 7. Set static files
+app.use(express.static('public'))
+
+// 3. Set route and handle request and response
+app.get('/', (req, res) => {
+  res.render('index', { restaurant: restaurantList })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  // find回傳搜尋到的值
+  // toString 型別轉換再判斷
+  const restaurants = restaurantList.find(restaurant => (restaurant.id).toString() === req.params.id)
+  res.render('show', { restaurant: restaurants })
+})
+
+app.get('/search', (req, res) => {
+  const keywords = req.query.keyword // 輸入搜尋欄的字
+  const keyword = req.query.keyword.toLowerCase().trim() // 進行比對的字串，轉換成小寫，並剔除空格
+  const restaurants = restaurantList.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword) ||
+    restaurant.category.includes(keyword)
+  })
+  // 新增搜尋不到餐廳
+  if (!restaurants.length) {
+    res.render('cannot_found', { restaurant: restaurantList, keywords })
+  } else {
+    res.render('index', { restaurant: restaurants, keywords })
+  }
+})
+
+// 4. Start and listen to server
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
