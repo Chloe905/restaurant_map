42829788c8306662a6cf9850ee5483970281fc06
Chloe905 <cchloehsieh@gmail.com>
2022-11-16 15:11:44 +0800

    feat: add search function

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-// 1. Insert Express by require
+// Insert Express、express-handlebars、mongoose、body-parser、
 const express = require('express')
-// 5. Insert express-handlebars by require
 const exphbs = require('express-handlebars')
-// 載入mongoose
 const mongoose = require('mongoose')
-// 載入 body-parser
 const bodyParser = require('body-parser')
+
 // 載入 restaurant model
 const Restaurant = require('./models/restaurant')
-const restaurant = require('./models/restaurant')
-
-// 2. Define server related variable
+// 2.Define server related variable
 const port = 3000
 const app = express()
 
@@ -32,11 +28,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 6. Set template Engine
+// Set template Engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// 7. Set static files
+// Set static files
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -48,16 +44,19 @@ app.get('/', (req, res) => {
     .then((restaurants) => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
+
 // 新增餐廳頁面
 app.get('/restaurants/new', (req, res) => {
   res.render('new')
 })
+
 // 新增餐廳
 app.post('/restaurants', (req, res) => {
   Restaurant.create(req.body)
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
+
 // 查看單一店詳情
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
@@ -66,6 +65,7 @@ app.get('/restaurants/:id', (req, res) => {
     .then(restaurant => res.render('detail', { restaurant }))
     .catch(err => console.log(err))
 })
+
 // 修改餐廳資訊
 app.get('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
@@ -90,23 +90,31 @@ app.post('/restaurants/:id/delete', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
+
+// 搜尋餐廳
 app.get('/search', (req, res) => {
   // 沒有輸入按送出，redirect 回根目錄
-  if (!req.query.keyword) {
-    return res.redirect('/')
-  }
-  const keywords = req.query.keyword // 輸入搜尋欄的字
+  if (!req.query.keyword) { return res.redirect('/') }
+
+  const keywords = req.query.keyword  // 輸入搜尋欄的字
   const keyword = req.query.keyword.toLowerCase().trim() // 進行比對的字串，轉換成小寫，並剔除空格
-  const restaurants = restaurantList.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword) ||
-      restaurant.category.toLowerCase().includes(keyword)
-  })
-  // 新增搜尋不到餐廳
-  if (!restaurants.length) {
-    res.render('cannot_found', { restaurant: restaurantList, keywords })
-  } else {
-    res.render('index', { restaurant: restaurants, keywords })
-  }
+
+  Restaurant.find()
+    .lean()
+    .then((restaurant) => {
+      const restaurants = restaurant.filter(data => {
+        return data.name.toLowerCase().includes(keyword) ||
+          data.category.toLowerCase().includes(keyword)
+      })
+
+      // 新增搜尋不到餐廳
+      if (!restaurants.length) {
+        res.render('cannot_found', { restaurant, keywords })
+      } else {
+        res.render('index', { restaurants, keywords })
+      }
+    })
+    .catch(err => console.log(err))
 })
 
 // 4. Start and listen to server
