7e53be5e1ce548c3794375aa3b68b7a970582965
Chloe905 <cchloehsieh@gmail.com>
2022-11-20 01:23:59 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-// 載入 restaurant model
-const Restaurant = require('./models/restaurant')
+
 // 載入路由
 const routes = require('./routes')
 // 2.Define server related variable
@@ -41,60 +40,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-// 搜尋餐廳
-app.get('/search', (req, res) => {
-  // 沒有輸入按送出，redirect 回根目錄
-  if (!req.query.keyword) { return res.redirect('/') }
-
-  const keywords = req.query.keyword // 輸入搜尋欄的字
-  const keyword = req.query.keyword.toLowerCase().trim() // 進行比對的字串，轉換成小寫，並剔除空格
-
-  const { sort } = req.query
-  const sortBy = {
-    default: { _id: 'asc' },
-    AtoZ: { name: 'asc' },
-    ZtoA: { name: 'desc' },
-    category: { category: 'asc' },
-    location: { location: 'asc' }
-  }
-  Restaurant.find()
-    .lean()
-    .sort(sortBy[sort])
-    .then((restaurant) => {
-      const restaurants = restaurant.filter(data => {
-        return data.name.toLowerCase().includes(keyword) ||
-          data.category.toLowerCase().includes(keyword)
-      })
-
-      // 新增搜尋不到餐廳
-      if (!restaurants.length) {
-        res.render('cannot_found', { restaurant, keywords })
-      } else {
-        res.render('index', { restaurants, keywords })
-      }
-    })
-    .catch(err => {
-      console.log(err)
-      res.render(
-        'errorPage',
-        { error: err.message })
-    })
-})
-
-// 排序餐廳
-app.get('/sort', (req, res) => {
-  console.log(req.query.sort)
-  Restaurant.find()
-    .lean()
-    .sort({ name: 'asc' })
-    .then((restaurants) => res.render('index', { restaurants }))
-    .catch(err => {
-      console.log(err)
-      res.render(
-        'errorPage',
-        { error: err.message })
-    })
-})
 // 4. Start and listen to server
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
---
+++ routes/modules/search.js
@@ -0,0 +1,50 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+// 載入 Restaurant models
+const Restaurant = require('../../models/restaurant')
+
+// 搜尋路由
+router.get('/', (req, res) => {
+  // 沒有輸入按送出，redirect 回根目錄
+  if (!req.query.keyword) { return res.redirect('/') }
+
+  const keywords = req.query.keyword // 輸入搜尋欄的字
+  const keyword = req.query.keyword.toLowerCase().trim() // 進行比對的字串，轉換成小寫，並剔除空格
+
+  const { sort } = req.query
+  const sortBy = {
+    default: { _id: 'asc' },
+    AtoZ: { name: 'asc' },
+    ZtoA: { name: 'desc' },
+    category: { category: 'asc' },
+    location: { location: 'asc' }
+  }
+
+  Restaurant.find()
+    .lean()
+    .sort(sortBy[sort])
+    .then((restaurant) => {
+      const restaurants = restaurant.filter(data => {
+        return data.name.toLowerCase().includes(keyword) ||
+          data.category.toLowerCase().includes(keyword)
+      })
+
+      // 新增搜尋不到餐廳
+      if (!restaurants.length) {
+        res.render('cannot_found', { restaurant, keywords })
+      } else {
+        res.render('index', { restaurants, keywords })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.render(
+        'errorPage',
+        { error: err.message })
+    })
+})
+
+// 匯出路由
+module.exports = router
